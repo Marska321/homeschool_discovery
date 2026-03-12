f8fd27cfd3c43cbbc522f2fdbde86c9a8efbc93d
Marska321 <mario.botha.mb@gmail.com>
2026-03-12 23:18:16 +0200

    Add blog section with markdown support and Vercel routing

---
+++ tmp_test.ts
@@ -0,0 +1,3 @@
+import { fetchPosts } from './src/hooks/use-posts.js'; // Can't easily import a TS React hook script and run it via node.
+
+// I will write a small vite script to test import.meta.glob.
